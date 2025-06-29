333a79f3750a531b6853ff9d7cd893ab0aac106b
kittibetti <kittibetti49@gmail.com>
2025-06-29 18:51:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -209,3 +209,9 @@ document.getElementById("easy").addEventListener("click", () => startGame("easy"
 document.getElementById("medium").addEventListener("click", () => startGame("medium"));
 document.getElementById("hard").addEventListener("click", () => startGame("hard"));
 document.getElementById("glitchkitti").addEventListener("click", () => startGame("glitch"));
+
+// Amikor az oldal betöltődik, aktiváljuk a sugarcute módot
+window.addEventListener('load', () => {
+  document.body.classList.add('sugarcute');
+});
+
